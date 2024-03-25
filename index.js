cf3f3424ec217f9156fdc233d1bfb6c4fc6df84e
zflorezz <zakinahf030904@gmail.com>
2024-03-24 22:57:44 -0500

    created index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+
+const app = express()
+app.use(bodyParser.json())
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const MONGOURL = process.env.MONGO_URL
+
+mongoose.connect(MONGOURL).then( ()=> {
+    console.log("Database Connected Successfully")
+
+    app.listen(PORT, ()=> {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch( error => console.log(error))
