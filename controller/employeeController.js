c4fea4344c8d714e6690b9bb818c2950e2a6e79a
zflorezz <zakinahf030904@gmail.com>
2024-04-01 20:14:49 -0500

    finding one employee is working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -63,3 +63,16 @@ export const update = async(req, res) => {
 //         res.status(500).json({error: "Internal Server Error"})
 //     }
 // }
+
+export const findEmployee = async(req, res) => {
+    try {
+        const id = req.params.id
+        const employeeExist = await Employee.findOne({_id:id})
+        if(!employeeExist) {
+            return res.status(404).json({message: "Employee does not exist"})
+        }
+        res.status(200).json(employeeExist)
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,10 +1,10 @@
 import express from "express"
-import {create, fetch, update} from "../controller/employeeController.js"
+import {create, fetch, update, findEmployee} from "../controller/employeeController.js"
 const route = express.Router()
 
 route.post("/create", create)
 route.get("/fetch", fetch)
 route.put("/update/:id", update)
 // route.delete("/delete/:id", deleteEmployee)
-
+route.get("/fetch/:id", findEmployee)
 export default route
